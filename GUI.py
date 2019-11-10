7ee694edc480b8ab6f51ffd489c7ccc4d293f9bb
AlexanderdeBont <57573396+AlexanderdeBont@users.noreply.github.com>
2019-11-10 11:55:03 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -22,16 +22,23 @@ def main():
     window = Tk()
     window.title("Readit beta v0.1")
     window.geometry('600x400')
+    mycolor = '#fa8366'
+    window.configure(background=mycolor)
     window.resizable(False, False)
 
+    secondlabel = Label(window, text='Readit beta v0.1', bg=mycolor).grid(row=1)
+    window.grid_rowconfigure(3, weight=1)
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_columnconfigure(3, weight=1)
 
-    label = Label(window, text='Enter a reddit link: ').grid(row=0)
-    url_entry = Entry(window)
-    url_entry.grid(row=0, column=1)
+    label = Label(window, text='Enter a reddit link: ', bg=mycolor).grid(row=1)
+
+    url_entry = Entry(window, width=50)
+    url_entry.grid(row=1, column=2)
     text = "hi"
 
 
-    enter = Button(window, text="Read It!", width=10, command=lambda : process_info(url_entry)).grid(row=1, column=1)
+    enter = Button(window, text="Read It!", width=10, command=lambda : process_info(url_entry)).grid(row=2, column=2)
 
     window.mainloop()
 
@@ -53,6 +60,8 @@ def main():
 
     #print(get_data(link, create_reddit_object()))
     #print(get_comments(link))
-
+    
 '''
 main()
+
+
